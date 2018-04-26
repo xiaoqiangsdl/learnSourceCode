5b07174fc0f08b096d096bdeaf93fdf5a3e84638
xiaoqiangsdl <602196490@qq.com>
2018-04-26 14:48:43 +0800

    feat: vue-init 基本注释

--- vue-cli/v2.0.0/lib/check-version.js
+++ vue-cli/v2.0.0/lib/check-version.js
@@ -3,11 +3,14 @@ var semver = require('semver')
 var chalk = require('chalk')
 
 module.exports = function (done) {
+  // @! 通过 https://registry.npmjs.org/:npm 来获取 npm 的详细信息
+  // 用于判断脚手架是否是最新的
   request({
     url: 'https://registry.npmjs.org/vue-cli',
     timeout: 1000
   }, function (err, res, body) {
     if (!err && res.statusCode === 200) {
+      // 最新版本信息在 data['dist-tags'].latset 下 
       var latestVersion = JSON.parse(body)['dist-tags'].latest
       var localVersion = require('../package.json').version
       if (semver.lt(localVersion, latestVersion)) {
